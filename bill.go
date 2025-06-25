b25b001a66025ecd9950b256a7a6269d16105246
saswanth07 <saswanth545@gmail.com>
2025-06-25 10:20:15 +0530

    Update bill.go

--- bill.go
+++ bill.go
@@ -1,24 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 type bill struct {
 	name  string
-	iteam map[string]float64
+	items map[string]float64
 	tip   float64
 }
 
-// new bill for do
-func newbill(name string) bill {
+// new bill making
+func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		iteam: map[string]float64{"cake":    7.90,
-		"bun":     8.90},
+		items: map[string]float64{},
 		tip:   0,
 	}
 	return b
 }
-
-
-// function to relate 
-func(b bill) format(){
-
+func (b *bill) format() string {
+	fs := "Bill Breakdown:\n"
+	var total float64
+	//list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v...$%v\n", k+":", v)
+		total += v
+	}
+	//tip
+	fs += fmt.Sprintf("%-25v...$%v\n", "tip:", b.tip)
+	//total
+	fs += fmt.Sprintf("%-25v...$%0.2f", "total:", total+b.tip)
+	return fs
+}
+func (b *bill) updatetip(tip float64) {
+	b.tip = tip
+}
+func (b *bill) itemsadd(name string, price float64) {
+	b.items[name] = price
+}
+func (b *bill) save(){
+	data:=[]byte(b.format())
+	err:=os.WriteFile("bills/"+b.name+".txt",data,0644)
+	if(err!=nil){
+		panic(err)
+	}
+	fmt.Println("bill saved to file")
 }
